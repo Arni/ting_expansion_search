dfe3f9e3f641eb0fedf59fc40fc87ba3c343f4f0
DQ26525 <DQ26525@LT8085.Randers.dk>
2019-05-16 15:58:22 +0200

    Initial commit

---
+++ templates/ting-expansion-search.tpl.php
@@ -0,0 +1,12 @@
+<?php ?>
+
+        
+<ul class="ting-expansion-search-links">
+    <?php foreach ($links as $link) : ?>
+      <li class="ting-expansion-search-link">
+          <?php print $link; ?> 
+      </li>
+
+    <?php endforeach; ?>
+</ul>
+
