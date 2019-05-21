4cdad213223c693bb6cff50b2c249ec954ca44a5
DQ26525 <DQ26525@LT8085.Randers.dk>
2019-05-21 14:42:15 +0200

    Added ajac functionality

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ting_expansion_search
+
+Fix error in curl multi module line 12. Add $options to foreach:
+
+  if(is_array($sessions)){
+    foreach($sessions as $session){
+        $options = array(
+    CURLOPT_RETURNTRANSFER => true, 
+    CURLOPT_FOLLOWLOCATION => true
+  );
--- js/ting-expansion-search.js
+++ js/ting-expansion-search.js
@@ -1,66 +1,52 @@
 (function($) {
-  "use strict";
+    $(document).ready(function() {
+//    Drupal.behaviors.tingSearchExpansion = {
+//        'attach': function(context) {
+            
+//          var request = $.ajax({
+//          url: Drupal.settings.tingSearchExpansion.url,
+////          type: 'POST',
+////          data: {
+////            coverData: cover_data
+////          },
+//          dataType: 'json',
+//          success: function(counts) {
+//              alert("1");
+//          }
+//          
+//          // Update processing state.
+////          complete: function(request, status) {
+////
+////          }
+//        }).once('tingSearchExpansion');
 
-  // Helper function to get information about a given cover place holder.
-  var ting_covers_extract_data = function(e) {
-    var id = $(e).data('ting-cover-object-id');
-    var image_style = $(e).data('ting-cover-style');
-    if (!id) {
-      return false;
-    }
-    return {
-      id : id,
-      image_style : image_style
-    };
-  };
-
-  var ting_cover_insert = function(covers) {
-    $.each(covers, function(index, cover_info) {
-      $('.ting-cover-processing[data-ting-cover-object-id="' + cover_info.id + '"][data-ting-cover-style="' + cover_info.image_style + '"]').html('<img src="' + cover_info.url + '"/>');
-    });
-  };
-
-  Drupal.behaviors.tingCovers = {
-    attach: function(context) {
-      // Assemble information regarding covers.
-      var cover_data = [];
-
-      // Extract cover information from the dom.
-      $('.ting-cover:not(.ting-cover-processing, .ting-cover-processed)', context).each(function(index, element) {
-        cover_data.push(ting_covers_extract_data(element));
-      }).addClass('ting-cover-processing');
-
-      if (cover_data.length > 0) {
-        //Retrieve covers
-        var request = $.ajax({
-          url: Drupal.settings.basePath + 'ting/covers',
-          type: 'POST',
-          data: {
-            coverData: cover_data
-          },
-          dataType: 'json',
-          success: ting_cover_insert,
-          // Update processing state.
-          complete: function(request, status) {
-            var processing = $('.ting-cover-processing', context);
-            if (status === 'success') {
-              processing.addClass('ting-cover-processed');
-            }
-            processing.removeClass('ting-cover-processing');
-          }
+            $.get(Drupal.settings.tingSearchExpansion.url, null, SearchExpansionResults);
+             //$( "#ting-search-expansion-link-ebog").spinner();
+//            $('a.photo_button:not(.mymodule-processed)', context)
+//                    .addClass('mymodule-processed')
+//                    .bind('click', function() {
+//                $.get('/photos/get/photos/' + parseInt(this.id, 10), null, imageDetails);
+//                return false;
+//            });
+//        }
+//    }
+//
+//    }
+      }); 
+    
+    var SearchExpansionResults= function(response) {
+        //console.log(response);
+        //alert("1");
+        $.each (response, function(key, value ) {
+            var text = $( "#ting-search-expansion-link-" + key ).text().replace('...', value); 
+            $( "#ting-search-expansion-link-" + key ).text(text);
+            //console.log(text);
+            //text = text.replace('...', value);
+            //console.log(text);
         });
-
-        // Associate the request with the context so we can abort the request if
-        // the context is detached removed before completion.
-        $(context).data('request', request);
-      }
-    },
-    detach: function(context) {
-      // If we have a request associated with the context then abort it.
-      var request = $(context).data('request');
-      if (request) {
-        request.abort();
-      }
     }
-  };
-}(jQuery));
+        
+        //$('div.field-type-image div.field-item').html(response.data);
+
+
+})(jQuery);
